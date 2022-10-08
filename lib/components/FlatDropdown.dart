0c7648ff2a4a77691e4c0607c9b86fe146194841
Marko R <clearfeld@outlook.com>
2022-10-08 14:57:49 -0400

    Add: flat dropdown component

---
+++ lib/components/FlatDropdown.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+typedef void SetValueStringCallback(String? value_arg);
+
+class FlatDropdown extends StatefulWidget {
+  const FlatDropdown({
+      super.key,
+
+      required this.value,
+      required this.onValueChanged,
+
+      required this.items,
+  });
+
+  final String value;
+  final SetValueStringCallback onValueChanged;
+
+  // TODO(clearfeld): allow for enums later
+  final List<String> items;
+
+  @override
+  State<FlatDropdown> createState() => _FlatDropdown();
+}
+
+class _FlatDropdown extends State<FlatDropdown> {
+
+  // Son({
+  //     @
+  // });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(41, 41, 41, 1.0), //background color of dropdown button
+        // border: Border.all(color: Colors.black38, width:3), //border of dropdown button
+        // borderRadius: BorderRadius.circular(50), //border raiuds of dropdown button
+        // boxShadow: <BoxShadow>[ //apply shadow on Dropdown button
+        //   BoxShadow(
+        //     color: Color.fromRGBO(0, 0, 0, 0.57), //shadow for button
+        //     blurRadius: 5) //blur radius of shadow
+        // ]
+      ),
+
+      child:Padding(
+        padding: EdgeInsets.only(left:30, right:30),
+
+        child: DropdownButton(
+          value: widget.value,
+          icon: const Icon(Icons.arrow_downward),
+          onChanged: (String? value_arg) {
+            widget.onValueChanged(value_arg);
+          },
+          items: widget.items.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+          }).toList(),
+
+          iconEnabledColor: Colors.white, //Icon color
+          style: TextStyle(  //te
+            color: Colors.white, //Font color
+            fontSize: 16 //font size on dropdown button
+          ),
+
+          dropdownColor: Colors.redAccent, //dropdown background color
+          underline: Container(), //remove underline
+          isExpanded: true, //make true to make width 100%
+        ),
+      ),
+    );
+  }
+}
