5a785e9f0cd8ea589356b1c27b22b95162c27edd
AaronPatterson1 <aaronpatters@gmail.com>
2022-12-05 20:03:56 -0500

    Add: Bar Graph Selector to display statistics for different periods.

--- lib/page_habit_specific_view/history_chart.dart
+++ lib/page_habit_specific_view/history_chart.dart
@@ -5,6 +5,7 @@ import 'package:habit_tracker/s_isar.dart';
 import 'package:habit_tracker/entities/habit_date.dart';
 
 import 'package:habit_tracker/page_habit_specific_view/history_bar_chart.dart';
+import 'package:intl/intl.dart';
 
 // import '../components/FlatDropdown.dart';
 
@@ -25,6 +26,15 @@ class HistoryChart extends ConsumerStatefulWidget {
 }
 
 class _HistoryChart extends ConsumerState<HistoryChart> {
+
+  String periodSelected = "Week";
+
+  var items = [
+    'Week',
+    'Month',
+    'Quarter',
+    'Year',
+  ];
   @override
   Widget build(BuildContext context) {
 
@@ -45,7 +55,7 @@ class _HistoryChart extends ConsumerState<HistoryChart> {
 
         children: <Widget>[
           Row(
-            children: const <Widget>[
+            children: <Widget>[
               Text(
                 "History",
                 style: TextStyle(
@@ -56,64 +66,142 @@ class _HistoryChart extends ConsumerState<HistoryChart> {
               Spacer(),
 
               Text(
-                "Selector - Week",
+                "Selector  -  ",
                 style: TextStyle(
                   fontSize: 20.0,
                 ),
               ),
+              Column(
+                children: [
+                  DropdownButton(
+                    // Initial Value
+                    dropdownColor: Color.fromRGBO(41, 41, 41, 1.0),
+                    value: periodSelected,
+
+                    // Down Arrow Icon
+                    icon: const Icon(Icons.keyboard_arrow_down),
+
+                    style: const TextStyle(
+                            fontSize: 20.0,
+                          ),
+
+                    // Array list of items
+                    items: items.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    // After selecting the desired option,it will
+                    // change button value to selected value
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        periodSelected = newValue!;
+                      });
+                    },
+                  ),
+                ],
+              )
             ],
           ),
 
           const SizedBox(height: 16.0,),
 
           FutureBuilder<List<HabitDate>>(
-            future: widget.isarService.getHabitsDateLastSeven(widget.habit.id),
+            future: widget.isarService.getHabitsDateLastSelectedPeriod(widget.habit.id, periodSelected),
 
             builder: (BuildContext context, AsyncSnapshot<List<HabitDate>> snapshot) {
               switch(snapshot.connectionState) {
                 case ConnectionState.done:
-
-                var d = DateTime.now();
-                var dstr = '${d.year}-${d.month}-';
-                List<dynamic> x = [
-                  dstr + (d.day).toString(),
-                  dstr + (d.day - 1).toString(),
-                  dstr + (d.day - 2).toString(),
-                  dstr + (d.day - 3).toString(),
-                  dstr + (d.day - 4).toString(),
-                  dstr + (d.day - 5).toString(),
-                  dstr + (d.day - 6).toString(),
-                ];
-                List<dynamic> xy = [];
-
                 var habitDates = snapshot.data;
-                if(habitDates != null) {
+                var d = DateTime.now();
 
-                  // sort by date
-                  habitDates.sort((a, b) {
-                      return a.getDate().compareTo(b.getDate());
-                  });
+                List<dynamic> xy = [];
+                List<dynamic> periods = [];
 
-                  for(var p in x) {
-                    var found = false;
-                    for(var v in habitDates) {
+                if (periodSelected == 'Week') {
+                  DateTime start = DateTime(d.year, d.month, d.day).subtract(Duration(days: d.weekday));
+                  DateTime end = start.add(Duration(days: 7));
 
-                      if(v.getDate() == p) {
-                        xy.add({
-                            "date": p,
-                            "object": v
-                        });
-                        found = true;
-                      }
+                  periods = [
+                    {'start': start.subtract(Duration(days: 28)), 'end': end.subtract(Duration(days: 28)), 'value' : 0},
+                    {'start': start.subtract(Duration(days: 21)), 'end': end.subtract(Duration(days: 21)), 'value' : 0},
+                    {'start': start.subtract(Duration(days: 14)), 'end': end.subtract(Duration(days: 14)), 'value' : 0},
+                    {'start': start.subtract(Duration(days: 7)), 'end': end.subtract(Duration(days: 7)), 'value' : 0},
+                    {'start': start, 'end': end, 'value' : 0},
+                  ];
+                }
+                else if (periodSelected == 'Month') {
+                  periods = [
+                    {'start': DateTime(d.year, d.month-4, 1), 'end': DateTime(d.year, d.month-3, 1), 'value' : 0, 'dateString': DateFormat('yMMMM').format(DateTime(d.year, d.month-4, 1))},
+                    {'start': DateTime(d.year, d.month-3, 1), 'end': DateTime(d.year, d.month-2, 1), 'value' : 0, 'dateString': DateFormat('yMMMM').format(DateTime(d.year, d.month-3, 1))},
+                    {'start': DateTime(d.year, d.month-2, 1), 'end': DateTime(d.year, d.month-1, 1), 'value' : 0, 'dateString': DateFormat('yMMMM').format(DateTime(d.year, d.month-2, 1))},
+                    {'start': DateTime(d.year, d.month-1, 1), 'end': DateTime(d.year, d.month, 1), 'value' : 0, 'dateString': DateFormat('yMMMM').format(DateTime(d.year, d.month-1, 1))},
+                    {'start': DateTime(d.year, d.month, 1), 'end': DateTime(d.year, d.month+1, 1), 'value' : 0, 'dateString': DateFormat('yMMMM').format(d)},
+                  ];
+                }
+                else if (periodSelected == 'Quarter') {
+                  DateTime start = DateTime(d.year, 1, 1);
+                  DateTime end = DateTime(d.year, 4, 1).subtract(Duration(days: 1));
+                  // Find the current quarter.
+                  int currentQuarter = 1;
+                  for (currentQuarter=1; currentQuarter<4; currentQuarter++) {
+                    if (d.isBefore(end) && d.isAfter(start)) {
+                      //current quarter found
+                      break;
                     }
+                    start = DateTime(start.year, start.month+3, 1);
+                    end = DateTime(end.year, start.month+3, 1).subtract(Duration(days: 1));
+                  }
 
-                    if(!found) {
-                      xy.add({
-                          "date": p,
-                          "object": null
-                      });
+                  periods = [{'start': start, 'end': end, 'value' : 0, 'dateString': '${d.year} Q$currentQuarter'}];
+                  for (var i=0; i<4; i++) {
+                    currentQuarter = currentQuarter - 1;
+                    if (currentQuarter == 0) {
+                      currentQuarter = 4;
+                    }
+                    end = start.subtract(Duration(days: 1));
+                    start = DateTime(start.year, start.month-3, 1);
+                    periods.insert(0, {'start': start, 'end': end, 'value' : 0, 'dateString': '${start.year} Q$currentQuarter'});
+                  }
+                }
+                else if (periodSelected == 'Year') {
+                  periods = [
+                    {'start': DateTime(d.year-4, 1, 1), 'end': DateTime(d.year-3, 1, 1), 'value' : 0, 'dateString': (d.year-4).toString()},
+                    {'start': DateTime(d.year-3, 1, 1), 'end': DateTime(d.year-2, 1, 1), 'value' : 0, 'dateString': (d.year-3).toString()},
+                    {'start': DateTime(d.year-2, 1, 1), 'end': DateTime(d.year-1, 1, 1), 'value' : 0, 'dateString': (d.year-2).toString()},
+                    {'start': DateTime(d.year-1, 1, 1), 'end': DateTime(d.year, 1, 1), 'value' : 0, 'dateString': (d.year-1).toString()},
+                    {'start': DateTime(d.year, 1, 1), 'end': DateTime(d.year+1, 1, 1), 'value' : 0, 'dateString': d.year.toString()},
+                  ];
+                }
+
+                for(var v in habitDates!) {
+                  var eventDate = v.getDate().split('-');
+                  int year = int.parse(eventDate[0]);
+                  int month = int.parse(eventDate[1]);
+                  int day = int.parse(eventDate[2]);
+
+                  for (var period in periods) {
+                    if ((DateTime(year, month, day).isAfter(period['start']) || period['start'] == DateTime(year, month, day)) && DateTime(year, month, day).isBefore(period['end'])) {
+                      period['value'] += v.getValue();
                     }
                   }
+                }
+                for (var period in periods) {
+                  String dateString = DateFormat('M-dd').format(period['start']) + ' to ' + DateFormat('M-dd').format(period['end']);
+                  if (periodSelected == 'Month') {
+                    dateString = period['dateString'];
+                  }
+                  else if (periodSelected == 'Quarter') {
+                    dateString = period['dateString'];
+                  }
+                  else if (periodSelected == 'Year') {
+                    dateString = period['dateString'];
+                  }
+                  xy.add({
+                    'date': dateString,
+                    'value': period['value']
+                  });
                 }
 
                 return HistoryBarChart(
@@ -121,7 +209,11 @@ class _HistoryChart extends ConsumerState<HistoryChart> {
                 );
 
                 default:
-                  return const Text("Loading");
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 260.0,
+                    child: const Text("Loading"),
+                  );
               }
             }
           ),
