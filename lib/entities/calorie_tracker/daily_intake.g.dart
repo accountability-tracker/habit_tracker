0d73e6dca8b56a733c55c6e39b148ae520b2744d
Marko R <clearfeld@outlook.com>
2023-05-09 21:35:44 -0400

    Add: line chart to habit specific view and update deps

--- lib/entities/calorie_tracker/daily_intake.g.dart
+++ lib/entities/calorie_tracker/daily_intake.g.dart
@@ -7,7 +7,7 @@ part of 'daily_intake.dart';
 // **************************************************************************
 
 // coverage:ignore-file
-// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters
+// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters, always_specify_types
 
 extension GetDailyIntakeCollection on Isar {
   IsarCollection<DailyIntake> get dailyIntakes => this.collection();
@@ -34,7 +34,7 @@ const DailyIntakeSchema = CollectionSchema(
   getId: _dailyIntakeGetId,
   getLinks: _dailyIntakeGetLinks,
   attach: _dailyIntakeAttach,
-  version: '3.0.5',
+  version: '3.1.0+1',
 );
 
 int _dailyIntakeEstimateSize(
