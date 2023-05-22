75f9ae5b046ca4daff1f4e7fc0eef3d5a330b92b
Marko <49600278+clearfeld@users.noreply.github.com>
2023-05-22 13:32:13 -0400

    Add: five day line is now sticky at the top while scrolling through habit items (#31)

--- lib/components/five_day_line.dart
+++ lib/components/five_day_line.dart
@@ -15,7 +15,6 @@ class _FiveDayLine extends ConsumerState<FiveDayLine> {
   @override
   void initState() {
     super.initState();
-    // ref.read(habitsManagerProvider);
   }
 
   var date = DateTime.now();
@@ -30,64 +29,37 @@ class _FiveDayLine extends ConsumerState<FiveDayLine> {
   @override
   Widget build(BuildContext context) {
     final customColors = Theme.of(context).extension<CustomColors>()!;
-    // final him = ref.watch(habitsManagerProvider);
 
-    return Container(
-      color: customColors.navbarBackground,
-      width: MediaQuery.of(context).size.width,
-      //margin: const EdgeInsets.all(2.0),
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          // Spacer(),
-
-          //Container(
-          //  width: MediaQuery.of(context).size.width * 0.45,
-          //),
-
-          // Text(date.toString()),
-          // Text(date.add(Duration(days: 1)).toString()),
-
-          SizedBox(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            width: (MediaQuery.of(context).size.width * 0.8) + 40,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                for (var n in nd)
-                  Column(
-                    children: <Widget>[
-                      Text(printDay(date.subtract(Duration(days: (4 - n))).weekday), style: TextStyle(color: customColors.textColorSecondary)),
-                      Text(date.subtract(Duration(days: (4 - n))).day.toString(), style: TextStyle(color: customColors.textColorSecondary)),
-                    ],
-                  ),
-              ],
-            ),
+    return Column(
+      children: [
+        Container(
+          color: customColors.navbarBackground,
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: (MediaQuery.of(context).size.width * 0.8) + 40,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    for (var n in nd)
+                      Column(
+                        children: <Widget>[
+                          Text(printDay(date.subtract(Duration(days: (4 - n))).weekday),
+                              style: TextStyle(color: customColors.textColorSecondary)),
+                          Text(date.subtract(Duration(days: (4 - n))).day.toString(),
+                              style: TextStyle(color: customColors.textColorSecondary)),
+                        ],
+                      ),
+                  ],
+                ),
+              ),
+            ],
           ),
-
-          // Expanded(
-          //   child: Align(
-          //     alignment: Alignment.centerRight,
-          //     child: Container(
-          //       child: Row(
-          //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //         children: <Widget>[
-
-          //           for(var n in  nd)
-          //           Column(
-          //             children: <Widget>[
-          //               Text(printDay(date.subtract(Duration(days: (4 - n))).weekday)),
-          //               Text(date.subtract(Duration(days: (4 - n))).day.toString()),
-          //             ],
-          //           )
-          //         ],
-          //       ),
-          //     ),
-          //   ),
-          // ),
-        ],
-      ),
+        ),
+        Divider(color: customColors.background, thickness: 4.0, height: 4),
+      ],
     );
   }
 }
